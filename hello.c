e2d501876a2b69b07a25f2926cd22415b25fa57a
Jankla <jankla2009@hotmail.com>
2018-03-27 01:08:42 +0700

    correct hello.c

--- hello.c
+++ hello.c
@@ -2,13 +2,7 @@
 void main()
 {
   printf("Hello World!\n");
-<<<<<<< HEAD
   printf("Why are you still here?");
-  printf("Ayy stop!");
-  printf("This is sickening.");
-=======
-  printf("Why are you still here?\n");
   printf("Every night. I can't feel my leg.\n");
-  printf("My arms.\n")
->>>>>>> 60fb1d598b8af4f7e6496a99869bfda613a1a798
+  printf("My arms.\n");
 }
