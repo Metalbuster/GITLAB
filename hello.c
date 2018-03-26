60fb1d598b8af4f7e6496a99869bfda613a1a798
Metalbuster <jankla2010@hotmail.com>
2018-03-27 00:33:49 +0700

    Metal Gear Reference in hello.c

--- hello.c
+++ hello.c
@@ -2,5 +2,7 @@
 void main()
 {
   printf("Hello World!\n");
-  printf("Why are you still here?");
+  printf("Why are you still here?\n");
+  printf("Every night. I can't feel my leg.\n");
+  printf("My arms.\n")
 }
